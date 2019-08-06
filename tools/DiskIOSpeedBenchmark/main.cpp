d8627006719f22c22aabce9d5cabd26c0e3556c5
Yuan Yao <yaoyuan0553@yahoo.com>
2019-08-06 17:02:47 +0800

    working on testing I/O read speed

--- tools/DiskIOSpeedBenchmark/main.cpp
+++ tools/DiskIOSpeedBenchmark/main.cpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <filesystem>
+#include <stdio.h>
 
 #include <XmlFileReader.h>
 #include <PatentTagTextCollector.h>
@@ -13,6 +15,7 @@
 
 
 using namespace std;
+namespace fs = std::filesystem;
 
 using OutputQueueByFile = unordered_map<string, vector<string>>;
 
@@ -52,7 +55,7 @@ class DoNothingCollector : public ThreadJob<> {
         }
     }
 public:
-    DoNothingCollector(CQueue<string>& filenameQueue) : filenameQueue_(filenameQueue) { }
+    explicit DoNothingCollector(CQueue<string>& filenameQueue) : filenameQueue_(filenameQueue) { }
 };
 
 
@@ -69,31 +72,136 @@ class DoNothingReader : public ThreadJob<> {
 
             stringstream buffer;
             buffer << ifs.rdbuf();
+
+            ifs.close();
         }
     }
 public:
-    DoNothingReader(CQueue<string>& filenameQueue) : filenameQueue_(filenameQueue) { }
+    explicit DoNothingReader(CQueue<string>& filenameQueue) : filenameQueue_(filenameQueue) { }
 };
 
 
+class DoNothingSyncCReader : public ThreadJob<> {
+    CQueue<string>& filenameQueue_;
+
+    inline static constexpr size_t DEFAULT_BUFSIZE = 1024;
+    size_t bufferSize_ = DEFAULT_BUFSIZE;
+    char* buffer_;
+
+    void internalRun() final
+    {
+        for (;;)
+        {
+            auto [filename, quit] = filenameQueue_.pop();
+            if (quit) break;
+
+            FILE* inputFile = fopen(filename.c_str(), "r");
+            if(!inputFile) {
+                cerr << "File opening failed\n";
+                continue;
+            }
+            /* Get the number of bytes */
+            fseek(inputFile, 0L, SEEK_END);
+            size_t numBytes = ftell(inputFile);
+
+            fseek(inputFile, 0L, SEEK_SET);
+
+            if (bufferSize_ < numBytes) {
+                free(buffer_);
+                bufferSize_ = numBytes;
+                buffer_ = (char*)malloc(bufferSize_);
+            }
+
+            fread(buffer_, sizeof(char), numBytes, inputFile);
+
+            fclose(inputFile);
+        }
+    }
+public:
+    explicit DoNothingSyncCReader(CQueue<string>& filenameQueue) : filenameQueue_(filenameQueue)
+    {
+        buffer_ = (char*)malloc(bufferSize_);
+    }
+
+    ~DoNothingSyncCReader() final
+    {
+        free(buffer_);
+    }
+};
+
+
+class DoNothingAsyncReader : public ThreadJob<> {
+    CQueue<string>& filenameQueue_;
+
+    inline static constexpr size_t DEFAULT_BUFSIZE = 1024;
+    size_t bufferSize_ = DEFAULT_BUFSIZE;
+    char* buffer_;
+
+    void internalRun() final
+    {
+        for (;;)
+        {
+            auto [filename, quit] = filenameQueue_.pop();
+            if (quit) break;
+
+            FILE* inputFile = fopen(filename.c_str(), "r");
+            if(!inputFile) {
+                cerr << "File opening failed\n";
+                continue;
+            }
+
+            fseek(inputFile, 0L, SEEK_END);
+            size_t numBytes = ftell(inputFile);
+
+            fseek(inputFile, 0L, SEEK_SET);
+
+            if (bufferSize_ < numBytes) {
+                free(buffer_);
+                bufferSize_ = numBytes;
+                buffer_ = (char*)malloc(bufferSize_);
+            }
+
+            fread(buffer_, sizeof(char), numBytes, inputFile);
+
+            fclose(inputFile);
+        }
+    }
+public:
+    explicit DoNothingAsyncReader(CQueue<string>& filenameQueue) :
+        filenameQueue_(filenameQueue)
+    {
+        buffer_ = (char*)malloc(bufferSize_);
+    }
+
+    ~DoNothingAsyncReader() final
+    {
+        free(buffer_);
+    }
+};
+
 
 class DiskIOSpeedBenchmark : public XmlPCProcessorInterface {
-    string pathFilename_;
+    string pathFilename1_;
+    string pathFilename2_;
 
-    CQueue<string> filenameQueue_;
+    CQueue<string> filenameQueue1_, filenameQueue2_;
 
     void prepareNodeFilters() final { }
     void prepareOutputFormatters() final { }
     void initializeData() final
     {
-        XmlFileReader xmlFileReader(pathFilename_, filenameQueue_);
+        XmlFileReader xmlFileReader(pathFilename1_, filenameQueue1_);
+        XmlFileReader xmlFileReader2(pathFilename2_, filenameQueue2_);
         xmlFileReader.runOnMain();
+        xmlFileReader2.runOnMain();
     }
 
     void initializeThreads() final
     {
-        for (int i = 0; i < nProducers_; i++)
-            producers_.add<DoNothingReader>(filenameQueue_);
+        for (int i = 0; i < nProducers_; i++) {
+            producers_.add<DoNothingReader>(filenameQueue1_);
+            producers_.add<DoNothingReader>(filenameQueue2_);
+        }
     }
 
     void executeThreads() final
@@ -103,26 +211,172 @@ class DiskIOSpeedBenchmark : public XmlPCProcessorInterface {
     }
 
 public:
-    DiskIOSpeedBenchmark(string_view pathFilename, int nFileReaders) :
-        pathFilename_(pathFilename)
+    DiskIOSpeedBenchmark(string_view pathFilename1, string_view pathFilename2, int nFileReaders) :
+        pathFilename1_(pathFilename1), pathFilename2_(pathFilename2)
     { nProducers_ = nFileReaders; }
 };
 
 void printUsageAndExit(const char* program)
 {
-    printf("Usage:\n\t\t%s <path-file> <num-threads>\n", program);
+    printf("Usage:\n\t\t%s <path-file-1> <path-file-2> <num-threads>\n", program);
     exit(-1);
 }
+
+/*
+int main(int argc, char* argv[])
+{
+    if (argc != 4)
+        printUsageAndExit(argv[0]);
+
+    DiskIOSpeedBenchmark diskIoSpeedBenchmark(argv[1], argv[2], atoi(argv[3]));
+
+    diskIoSpeedBenchmark.process();
+
+    return 0;
+}
+*/
+
+class PathFinder : public ThreadJob<CQueue<string>&> {
+
+    vector<fs::path> pathRoots_;
+    size_t batchSize_;
+
+    void internalRun(CQueue<string>& filepathQueue) final
+    {
+        vector<string> fileBatch;
+        fileBatch.reserve(batchSize_);
+
+        for (const fs::path& root : pathRoots_) {
+            if (!fs::exists(root))
+                fprintf(stderr, "path [%s] does not exist\n", root.c_str());
+            for (const fs::directory_entry& entry : fs::recursive_directory_iterator(root)) {
+                if (fs::is_regular_file(entry.path())) {
+                    fileBatch.push_back(entry.path());
+                }
+
+                if (fileBatch.size() == batchSize_) {
+                    filepathQueue.push(fileBatch);
+
+                    fileBatch.clear();
+                }
+            }
+            if (!fileBatch.empty()) {
+                filepathQueue.push(fileBatch);
+                fileBatch.clear();
+            }
+        }
+        filepathQueue.setQuitSignal();
+    }
+public:
+    PathFinder(vector<fs::path>&& pathRoots, CQueue<string>& filepathQueue,
+            size_t batchSize = 128) :
+        ThreadJob(filepathQueue), pathRoots_(pathRoots), batchSize_(batchSize) { }
+};
+
+class DiskIOBenchmarkWithPathFinder : public XmlPCProcessorInterface {
+    string pathRoot_;
+
+    CQueue<string> filenameQueue_;
+
+    void prepareNodeFilters() final { }
+    void prepareOutputFormatters() final { }
+    void initializeData() final
+    {
+        PathFinder pathFinder(
+                {pathRoot_ + "/2015", pathRoot_ + "/2016"}, filenameQueue_);
+        pathFinder.runOnMain();
+    }
+
+    void initializeThreads() final
+    {
+        for (int i = 0; i < nProducers_; i++) {
+            producers_.add<DoNothingReader>(filenameQueue_);
+        }
+    }
+
+    void executeThreads() final
+    {
+        producers_.runAll();
+        producers_.waitAll();
+    }
+
+public:
+    DiskIOBenchmarkWithPathFinder(string_view pathRoot, int nFileReaders) :
+            pathRoot_(pathRoot)
+    { nProducers_ = nFileReaders; }
+};
+
+
+class DiskIOBenchmarkWithCRead : public XmlPCProcessorInterface {
+    string pathFilename_;
+
+    CQueue<string> filenameQueue_;
+
+    void prepareNodeFilters() final { }
+    void prepareOutputFormatters() final { }
+    void initializeData() final
+    {
+        XmlFileReader xmlFileReader(pathFilename_, filenameQueue_);
+
+        xmlFileReader.runOnMain();
+    }
+
+    void initializeThreads() final
+    {
+        for (int i = 0; i < nProducers_; i++) {
+            producers_.add<DoNothingReader>(filenameQueue_);
+        }
+    }
+
+    void executeThreads() final
+    {
+        producers_.runAll();
+        producers_.waitAll();
+    }
+
+public:
+    DiskIOBenchmarkWithCRead(string_view pathFilename, int nFileReaders) :
+            pathFilename_(pathFilename)
+    { nProducers_ = nFileReaders; }
+};
+
+
+void printUsageAndExit2(const char* program)
+{
+    printf("Usage:\n\t\t%s <root-path> <num-threads>\n", program);
+    exit(-1);
+}
+
+void singleLargeFileBenchmark(const char* filename)
+{
+    FILE* inputFile = fopen(filename, "r");
+    if(!inputFile) {
+        cerr << "File opening failed\n";
+        return;
+    }
+    /* Get the number of bytes */
+    fseek(inputFile, 0L, SEEK_END);
+    size_t numBytes = ftell(inputFile);
+
+    fseek(inputFile, 0L, SEEK_SET);
+
+    char* buffer = (char*)malloc(numBytes);
+
+    fread(buffer, sizeof(char), numBytes, inputFile);
+
+    fclose(inputFile);
+}
+
 
 int main(int argc, char* argv[])
 {
     if (argc != 3)
-        printUsageAndExit(argv[0]);
+        printUsageAndExit2(argv[0]);
 
-    string pathFilename(argv[1]);
-    DiskIOSpeedBenchmark diskIoSpeedBenchmark(pathFilename, atoi(argv[2]));
+    DiskIOBenchmarkWithCRead diskIoBenchmarkWithCRead(argv[1], atoi(argv[2]));
 
-    diskIoSpeedBenchmark.process();
+    diskIoBenchmarkWithCRead.process();
+//    singleLargeFileBenchmark(argv[1]);
 
     return 0;
 }
