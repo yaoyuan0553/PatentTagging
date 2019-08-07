5ab754a49c681c7e062d92857095604b92a9ccac
Yuan Yao <yaoyuan0553@yahoo.com>
2019-08-07 21:19:40 +0800

    working on io_submit async

--- tools/DiskIOSpeedBenchmark/main.cpp
+++ tools/DiskIOSpeedBenchmark/main.cpp
@@ -13,6 +13,9 @@
 #include <sys/fcntl.h>
 #include <unistd.h>
 #include <aio.h>
+#include <linux/aio_abi.h>
+#include <poll.h>
+#include <sys/syscall.h>
 
 #include <XmlFileReader.h>
 #include <PatentTagTextCollector.h>
@@ -569,6 +572,154 @@ void singleLargeFileAioReadBenchmark(const char* filename)
     delete[] buffer;
 }
 
+#ifndef IOCB_CMD_POLL
+#define IOCB_CMD_POLL 5
+#endif
+
+inline static int io_setup(unsigned nr, aio_context_t *ctxp)
+{
+    return syscall(__NR_io_setup, nr, ctxp);
+}
+
+inline static int io_destroy(aio_context_t ctx)
+{
+    return syscall(__NR_io_destroy, ctx);
+}
+
+inline static int io_submit(aio_context_t ctx, long nr, struct iocb **iocbpp)
+{
+    return syscall(__NR_io_submit, ctx, nr, iocbpp);
+}
+
+
+#define AIO_RING_MAGIC 0xa10a10a1
+struct aio_ring {
+    unsigned id; /** kernel internal index number */
+    unsigned nr; /** number of io_events */
+    unsigned head;
+    unsigned tail;
+
+    unsigned magic;
+    unsigned compat_features;
+    unsigned incompat_features;
+    unsigned header_length; /** size of aio_ring */
+
+    struct io_event events[0];
+};
+
+/* Stolen from kernel arch/x86_64.h */
+#ifdef __x86_64__
+#define read_barrier() __asm__ __volatile__("lfence" ::: "memory")
+#else
+#ifdef __i386__
+#define read_barrier() __asm__ __volatile__("" : : : "memory")
+#else
+#define read_barrier() __sync_synchronize()
+#endif
+#endif
+
+/* Code based on axboe/fio:
+ * https://github.com/axboe/fio/blob/702906e9e3e03e9836421d5e5b5eaae3cd99d398/engines/libaio.c#L149-L172
+ */
+inline static int io_getevents(aio_context_t ctx, long min_nr, long max_nr,
+                               struct io_event *events,
+                               struct timespec *timeout)
+{
+    int i = 0;
+
+    aio_ring *ring = (struct aio_ring *)ctx;
+    if (ring == NULL || ring->magic != AIO_RING_MAGIC) {
+        goto do_syscall;
+    }
+
+    while (i < max_nr) {
+        unsigned head = ring->head;
+        if (head == ring->tail) {
+            /* There are no more completions */
+            break;
+        } else {
+            /* There is another completion to reap */
+            events[i] = ring->events[head];
+            read_barrier();
+            ring->head = (head + 1) % ring->nr;
+            i++;
+        }
+    }
+
+    if (i == 0 && timeout != NULL && timeout->tv_sec == 0 &&
+        timeout->tv_nsec == 0) {
+        /* Requested non blocking operation. */
+        return 0;
+    }
+
+    if (i && i >= min_nr) {
+        return i;
+    }
+
+    do_syscall:
+    return syscall(__NR_io_getevents, ctx, min_nr - i, max_nr - i,
+                   &events[i], timeout);
+}
+
+#define PFATAL(x...)                                                           \
+	do {                                                                   \
+		fprintf(stderr, "[-] SYSTEM ERROR : " x);                      \
+		fprintf(stderr, "\n\tLocation : %s(), %s:%u\n", __FUNCTION__,  \
+			__FILE__, __LINE__);                                   \
+		perror("      OS message ");                                   \
+		fprintf(stderr, "\n");                                         \
+		exit(EXIT_FAILURE);                                            \
+	} while (0)
+
+
+void singleLargeFileIoSubmitBenchmark(const char* filename)
+{
+    size_t size = getFileSize(filename);
+
+    cout << size << '\n';
+
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "Cannot open [%s]\n", filename);
+        exit(-1);
+    }
+
+    char* buffer = new char[size];
+
+    iocb cb1;
+    memset(&cb1, 0, sizeof(iocb));
+    cb1.aio_nbytes = size;
+    cb1.aio_fildes = fd;
+    cb1.aio_lio_opcode = IOCB_CMD_PREAD;
+    cb1.aio_buf = (__u64)buffer;
+
+    iocb cb2;
+    memset(&cb2, 0, sizeof(iocb));
+
+
+    iocb* iocbList[1] = {&cb1};
+
+    aio_context_t ctx = 0;
+    int ret = io_setup(128, &ctx);
+    if (ret < 0)
+        PFATAL("io_setup()");
+
+    ret = io_submit(ctx, 1, iocbList);
+    if (ret != 1)
+        PFATAL("io_submit()");
+
+    io_event events[1] = {};
+    ret = io_getevents(ctx, 1, 1, events, NULL);
+    if (ret != 1)
+        PFATAL("io_getevents()");
+
+    io_destroy(ctx);
+    close(fd);
+
+    delete[] buffer;
+}
+
+
 
 int main(int argc, char* argv[])
 {
@@ -580,7 +731,7 @@ int main(int argc, char* argv[])
 
     diskIoBenchmarkWithCRead.process();
 */
-    singleLargeFileAioReadBenchmark(argv[1]);
+    singleLargeFileIoSubmitBenchmark(argv[1]);
 
     return 0;
 }
