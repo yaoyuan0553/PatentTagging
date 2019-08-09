978e4e405bdca7f6139e804d4918d3763b9d9b1a
Yuan Yao <yaoyuan0553@yahoo.com>
2019-08-09 16:14:15 +0800

    init commit of new-patent-format

---
+++ tools/EngPatentInfoExtractor/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by yuan on 8/9/19.
+//
+
+#include "Shared.h"
+
+
+int main()
+{
+
+    return 0;
+}
---
+++ tools/shared/include/Shared.h
@@ -0,0 +1,23 @@
+//
+// Created by yuan on 8/9/19.
+//
+
+#pragma once
+#ifndef TOOLS_XMLPROCESSOR_H
+#define TOOLS_XMLPROCESSOR_H
+
+#include "Utility.h"
+#include "ConcurrentQueue.h"
+#include "ThreadJob.h"
+#include "ThreadPool.h"
+#include "FunctorDict.h"
+#include "TagNodeFilterFunctors.h"
+#include "FormatFunctors.h"
+#include "PatentInfoWriter.h"
+#include "PatentTagTextCollector.h"
+#include "StatsThread.h"
+#include "XmlFileReader.h"
+#include "XmlPCProcessorInterface.h"
+#include "XmlTagTextWalker.h"
+
+#endif //TOOLS_XMLPROCESSOR_H
