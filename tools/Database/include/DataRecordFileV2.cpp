633c72c75635dbb1f0590e1736585befc9bf163a
yaoyuan0553 <yaoyuan0553@yahoo.com>
2019-09-02 18:14:52 -0500

    working on DataRecordFile

--- tools/Database/include/DataRecordFileV2.cpp
+++ tools/Database/include/DataRecordFileV2.cpp
@@ -4,24 +4,30 @@
 
 #include "DataRecordFileV2.h"
 
+using namespace std;
+
+DataRecordFileV2::DataRecordFileV2(uint64_t sizeToAllocate) :
+        buf_(new char[max(sizeToAllocate, FILE_HEAD_SIZE)]),
+        nBytes_(*(uint64_t*)buf_),
+        nRecords_(*(uint32_t*)(buf_ + sizeof(decltype(nBytes_)))),
+        capacity_(sizeToAllocate)
+{
+    nBytes_ = FILE_HEAD_SIZE;   // only 12 bytes are used
+    nRecords_ = 0;
+}
 
 DataRecordFileV2::~DataRecordFileV2()
 {
     delete[] buf_;
 }
 
-DataRecordFileV2::DataRecordFileV2(uint64_t sizeToAllocate) :
-        buf_(new char[sizeToAllocate]),
-        nBytes_(*(uint64_t*)buf_),
-        nRecords_(*(uint32_t*)(buf_ + sizeof(decltype(nBytes_))))
-{
-    nBytes_ = FILE_HEAD_SIZE;   // only 12 bytes are used
-    nRecords_ = 0;
-}
-
-
 DataRecordFileWriter::DataRecordFileWriter(size_t maxFileSize) :
         DataRecordFileV2(maxFileSize), curBuf_(buf_), maxFileSize_(maxFileSize)
+{
+    obtainBinId();
+}
+
+void DataRecordFileWriter::writeToFile(const char* filename)
 {
 
 }
--- tools/Database/include/DataRecordFileV2.h
+++ tools/Database/include/DataRecordFileV2.h
@@ -20,24 +20,35 @@ protected:
     /*! keeps track of number of records stored in buf_ */
     uint32_t& nRecords_;
 
-    uint64_t allocatedSize_;
+    uint64_t capacity_;
 
     inline static constexpr auto FILE_HEAD_SIZE =
             sizeof(decltype(nBytes_)) +
             sizeof(decltype(nRecords_));
+
     /**
      * @brief initializes the file to be only the headers
      * @details headers contains an 8 byte unsigned integer - nBytes and
      *          a 4 byte unsigned integer - nRecords
      *          which means the buf_ will only be 12 bytes initially
      */
-    explicit DataRecordFileV2(uint64_t sizeToAllocate);
+    explicit DataRecordFileV2(uint64_t sizeToAllocate = 0);
 
 public:
-    /**
-     * @brief releases all memory allocated under buf_
-     */
+    /*! @brief releases all memory allocated under buf_ */
     ~DataRecordFileV2();
+
+    /*! total bytes allocated */
+    [[nodiscard]] inline uint32_t capacity() const { return capacity_; }
+
+    /*! total bytes of data written to the buffer */
+    [[nodiscard]] inline uint32_t numBytes() const { return nBytes_; }
+
+    /*! current number of records in the buffer */
+    [[nodiscard]] inline uint32_t numRecords() const { return nRecords_; }
+
+    /* returns true if number of records is 0 */
+    [[nodiscard]] inline bool empty() const { return nRecords_ == 0; }
 };
 
 
@@ -55,9 +66,7 @@ class DataRecordFileWriter : DataRecordFileV2, public FileWritable {
      */
     uint32_t binId_;
 
-    /**
-     * points to current buffer location writing to
-     */
+    /*! points to current buffer location writing to */
     char* curBuf_;
 
     /*! stores the corresponding index value list and writes to disk */
@@ -66,15 +75,21 @@ class DataRecordFileWriter : DataRecordFileV2, public FileWritable {
     /*! max number of bytes in the file */
     uint64_t maxFileSize_;
 
-    /**
-     * obtains a new bin Id for a newly created instance
-     */
+    /*! obtains a new bin Id for a newly created instance */
     inline void obtainBinId();
 
 public:
 
     explicit DataRecordFileWriter(size_t maxFileSize);
 
+    [[nodiscard]] inline const IndexValueList& indexValueList() const { return indexValueList_; }
+
+    inline std::string generateFilename(const char* prefix) const
+    {
+        return std::string(prefix) + "_" + std::to_string(binId_) + ".bin";
+    }
+
+    void writeToFile(const char* filename) final;
 };
 
 inline void DataRecordFileWriter::obtainBinId()
